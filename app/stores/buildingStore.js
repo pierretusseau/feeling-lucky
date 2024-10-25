de02cd5d16885a5b1f087251e4f2ead551a19c80
PierreT <pierre.tusseau@gmail.com>
2024-10-25 15:38:19 +0200

    Data persistence on reload

--- app/stores/buildingStore.js
+++ app/stores/buildingStore.js
@@ -1,17 +1,27 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-const useBuildingStore = create((set) => ({
-  buildings: [],
-  add: (spec) => set(
-    (state) => ({ buildings: [...state.buildings, spec] })
-  ),
-  remove: (spec) => set(
-    (state) => {
-      const newBuildings = state.buildings.filter(s => s.name !== spec.name)
-      return { buildings: newBuildings }
+const useBuildingStore = create(
+  persist(
+    (set) => ({
+      buildings: [],
+      add: (spec) => set(
+        (state) => ({ buildings: [...state.buildings, spec] })
+      ),
+      remove: (spec) => set(
+        (state) => {
+          const newBuildings = state.buildings.filter(s => s.name !== spec.name)
+          return { buildings: newBuildings }
+        }
+      ),
+      reset: () => set({ buildings: [] }),
+    }),
+    {
+      name: 'flh-buildings', // name of the item in the storage (must be unique)
+      // storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
     }
-  ),
-  reset: () => set({ buildings: [] }),
-}))
+  )
+)
+
 
 export default useBuildingStore
--- app/stores/speciesStore.js
+++ app/stores/speciesStore.js
@@ -1,17 +1,26 @@
 import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-const useSpeciesStore = create((set) => ({
-  species: [{name: "Humans", slug: "humans"}],
-  addSpecies: (spec) => set(
-    (state) => ({ species: [...state.species, spec] })
-  ),
-  removeSpecies: (spec) => set(
-    (state) => {
-      const newSpecies = state.species.filter(s => s.slug !== spec.slug)
-      return { species: newSpecies }
+const useSpeciesStore = create(
+  persist(
+    (set) => ({
+      species: [{name: "Humans", slug: "humans"}],
+      addSpecies: (spec) => set(
+        (state) => ({ species: [...state.species, spec] })
+      ),
+      removeSpecies: (spec) => set(
+        (state) => {
+          const newSpecies = state.species.filter(s => s.slug !== spec.slug)
+          return { species: newSpecies }
+        }
+      ),
+      reset: () => set({ species: [{name: "Humans", slug: "humans"}] }),
+    }),
+    {
+      name: 'flh-species', // name of the item in the storage (must be unique)
+      // storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
     }
-  ),
-  reset: () => set({ species: [{name: "Humans", slug: "humans"}] }),
-}))
+  )
+)
 
 export default useSpeciesStore
