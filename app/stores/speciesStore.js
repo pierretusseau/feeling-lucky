2d3c54a447ce94486aad75c08c38937e22bf9c2f
PierreT <pierre.tusseau@gmail.com>
2024-10-29 17:32:06 +0100

    Removes unused imports

--- app/stores/speciesStore.js
+++ app/stores/speciesStore.js
@@ -1,5 +1,8 @@
 import { create } from 'zustand'
-import { persist, createJSONStorage } from 'zustand/middleware'
+import {
+  persist,
+  // createJSONStorage
+} from 'zustand/middleware'
 
 const useSpeciesStore = create(
   persist(
